b2abb63703ba4333411999c582fd73b19e0c6944
kristina15 <krisy-15012001@yandex.ru>
2021-02-19 14:20:49 +0400

    Uncomment

--- Program.cs
+++ Program.cs
@@ -10,9 +10,9 @@ namespace Barabasi_Albert_Model_Generator
             Graph graph = new Graph();
             while (!graph.GatherInput())
                 ;
-            //graph.Barabasi_Albert();
-            //double p = 0.4;
-            //graph.Erdos_Renyi(p);
+            graph.Barabasi_Albert();
+            double p = 0.4;
+            graph.Erdos_Renyi(p);
             graph.Bollobas_Riordan();
             Image img = graph.generateImage();
             img.Save(@"C:\Users\krisy\OneDrive\Documents\SiAKOD\GraphGenerator\Новый точечный рисунок.jpg");
